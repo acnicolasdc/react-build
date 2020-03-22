e764240ebd9e16af5e87558d094f644a10c63f88
JFuenmayorLT <jfuenmayor@lean-tech.io>
2020-03-21 21:29:57 -0500

    Updated Form to receive onSubmit function

--- src/components/Form/FormBody/FormBody.jsx
+++ src/components/Form/FormBody/FormBody.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { FormContainer } from './styles';
+import { FormContainer } from './styles'
 
-const FormBody = ({ render }) => {
-    const _renderInputs = () => {
-        return render.map( (children, index) => (
-            <div className ="form-inputs" key={index}>{children}</div>
-        ));
-    }
-    return (
-        <FormContainer>
-            <fieldset>
-                {_renderInputs()}
-            </fieldset>
-        </FormContainer>
-    )
+const FormBody = ({ render, onSubmit }) => {
+  const _renderInputs = () => {
+    return render.map((children, index) => (
+      <div className='form-inputs' key={index}>
+        {children}
+      </div>
+    ))
+  }
+  return (
+    <FormContainer onSubmit={onSubmit}>
+      <fieldset>{_renderInputs()}</fieldset>
+    </FormContainer>
+  )
 }
 
-export default FormBody;
+export default FormBody
--- src/components/Form/FormBody/index.js
+++ src/components/Form/FormBody/index.js
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types'
-import FormBody from './FormBody';
-import ButtonGeneric from '../../Button/ButtonGeneric';
+import FormBody from './FormBody'
+import ButtonGeneric from '../../Button/ButtonGeneric'
 
 FormBody.propTypes = {
-    render: PropTypes.arrayOf(PropTypes.element).isRequired,
-    Button: PropTypes.func.isRequired,
-    onSend: PropTypes.func.isRequired,
-};
+  render: PropTypes.arrayOf(PropTypes.element).isRequired,
+  Button: PropTypes.func.isRequired,
+  onSend: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
 
 FormBody.defaultProps = {
-    render: [],
-    Button: ButtonGeneric,
-    onSend: () => console.log('Without function'),
-};
-export default FormBody;
+  render: [],
+  Button: ButtonGeneric,
+  onSend: () => console.log('Without function'),
+  onSubmit: e => {
+    e.preventDefault()
+  }
+}
+export default FormBody
