a7cc655f8184ee4309b73bd05766a41f6ca11362
JFuenmayorLT <jfuenmayor@lean-tech.io>
2020-03-21 22:32:55 -0500

    Form props updated, removed redundant function onSend

--- src/components/Form/FormBody/index.js
+++ src/components/Form/FormBody/index.js
@@ -1,20 +1,18 @@
-import PropTypes from 'prop-types'
-import FormBody from './FormBody'
-import ButtonGeneric from '../../Button/ButtonGeneric'
+import PropTypes from 'prop-types';
+import FormBody from './FormBody';
+import ButtonGeneric from '../../Button/ButtonGeneric';
 
 FormBody.propTypes = {
   render: PropTypes.arrayOf(PropTypes.element).isRequired,
   Button: PropTypes.func.isRequired,
-  onSend: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired
-}
+};
 
 FormBody.defaultProps = {
   render: [],
   Button: ButtonGeneric,
-  onSend: () => console.log('Without function'),
   onSubmit: e => {
-    e.preventDefault()
+    e.preventDefault();
   }
-}
-export default FormBody
+};
+export default FormBody;
